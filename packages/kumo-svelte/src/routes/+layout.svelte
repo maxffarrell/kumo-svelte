3338bec1c61dc96227cd7cf9c01a98dd41872b0f
Max Farrell <maxffarrell@gmail.com>
2026-08-04 16:18:06 -0500

    Port upstream 6bdbeeb3 dark mode favicon

--- packages/kumo-svelte/src/routes/+layout.svelte
+++ packages/kumo-svelte/src/routes/+layout.svelte
@@ -12,6 +12,8 @@
   <meta name="build-date" content={__BUILD_DATE__} />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="color-scheme" content="light dark" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" media="(prefers-color-scheme: light)" />
+  <link rel="icon" type="image/svg+xml" href="/favicon-white.svg" media="(prefers-color-scheme: dark)" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=optional" />

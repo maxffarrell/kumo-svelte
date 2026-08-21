ec3af3742dda1e1c23ea571b5555601224d838f2
Max Farrell <maxffarrell@gmail.com>
2026-08-21 17:10:14 -0400

    fix(overlays): normalize shadows and outlines

--- packages/kumo-svelte/src/lib/docs/HomeGrid.svelte
+++ packages/kumo-svelte/src/lib/docs/HomeGrid.svelte
@@ -357,7 +357,7 @@
                                     id="home-tooltip-add"
                                     role="tooltip"
                                     data-side="top"
-                                    class="kumo-tooltip-popup absolute bottom-[calc(100%+10px)] left-1/2 z-10 flex -translate-x-1/2 flex-col rounded-md bg-kumo-base px-2.5 py-1.5 text-sm whitespace-nowrap text-kumo-default shadow-lg shadow-kumo-tip-shadow outline outline-kumo-fill"
+                                    class="kumo-tooltip-popup absolute bottom-[calc(100%+10px)] left-1/2 z-10 flex -translate-x-1/2 flex-col rounded-md bg-kumo-base px-2.5 py-1.5 text-sm whitespace-nowrap text-kumo-default shadow-md outline outline-kumo-line"
                                 >
                                     Add
                                     <span
@@ -377,11 +377,11 @@
                                             />
                                             <path
                                                 d="M8.99542 1.85876C9.75604 1.17425 10.9106 1.17422 11.6713 1.85878L16.5281 6.22989C17.0789 6.72568 17.7938 7.00001 18.5349 7.00001L15.89 7L11.0023 2.60207C10.622 2.2598 10.0447 2.2598 9.66436 2.60207L4.77734 7L2.13171 7.00001C2.87284 7.00001 3.58774 6.72568 4.13861 6.22989L8.99542 1.85876Z"
-                                                class="fill-kumo-tip-shadow"
+                                                class="fill-kumo-arrow-edge"
                                             />
                                             <path
                                                 d="M10.3333 3.34539L5.47654 7.71648C4.55842 8.54279 3.36693 9 2.13172 9H0V8H2.13172C3.11989 8 4.07308 7.63423 4.80758 6.97318L9.66437 2.60207C10.0447 2.25979 10.622 2.2598 11.0023 2.60207L15.8591 6.97318C16.5936 7.63423 17.5468 8 18.5349 8H20V9H18.5349C17.2997 9 16.1082 8.54278 15.1901 7.71648L10.3333 3.34539Z"
-                                                class="fill-kumo-tip-stroke"
+                                                class="fill-kumo-arrow-stroke"
                                             />
                                         </svg>
                                     </span>
